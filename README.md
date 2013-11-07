bb066bd30080f8f89a806017c268e38841ff159a
Noah Cooper <noah.cooper@hotmail.com>
2013-11-07 08:58:25 -0500

    v0.2

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 MyWednesdayFix
 ==============
 
-Version: 0.1 (06-NOV-2013)
+Version: 0.2 (07-NOV-2013)
+
+MyWednesdayFix is an app currently under development. It will be available as a web app, and as a native app for iOS and Android devices.
+
+The app is for comic book nerds by a comic book nerd. Its purpose is to provide said nerds with a look at new comic books in stores this Wednesday (or some other day this week for those odd books that go on sale on a Friday), as well as a look further into the future at books that are coming soon. It will also provide a simple way to lookup the closest comic book store in the nerd's area.
+
+The app is built using:
+
+ * [jQuery](http://jquery.com) for all sorts of JavaScript hackery
+ * [Bootstrap](http://getbootstrap.com) for UI components
+ * The [ComicVine API](http://comicvine.com/api) for book data
+ * [PhoneGap](http://phonegap.com) for iOS and Android native apps
--- src/js/app.js
+++ src/js/app.js
@@ -1,84 +1,260 @@
 /* 
  * MyWednesdayFix - app.js
- * Version: 0.1 (06-NOV-2013) 
+ * Version: 0.2 (07-NOV-2013) 
  */
 
 (function($) {
+  'use strict';
+  
   /* establish namespace for the app */
   var myWednesdayFix = {};
   
-  /* view handlers */
-  myWednesdayFix.view = {
-    thisWeek: function() {
-      $('#content-headline').html('New This Week');
+  /* state info and other app data */
+  myWednesdayFix.data = {
+    currentIssue: {}
+  };
+  
+  /* comicvine api methods */
+  myWednesdayFix.comicVine = {
+    /* get a list of issues sorted by store_date */
+    getIssuesByStoreDate: function(options) {
+      var settings = $.extend({
+        limit: '24', 
+        callback: $.noop
+      }, options || {});
       
-      $('#content-wrap').html('');
-      
-      $('#content-loading').show();
-      
-      /* TODO: move out of view function */
-      /* TODO: pagination */
-      /* TODO: automatically paginate when list includes dates with non-matching store_date */
-      /* TODO: account for comics not published on wednesday using date range for full week */
-      /* TODO: account for possibility of undefined field values */
       $.ajax({
         dataType: 'jsonp', 
         url: 'http://www.comicvine.com/api/issues' + 
              '?api_key=e21e187f67061d346687aeb81969a3fd2d1676fa' + 
              '&field_list=api_detail_url,store_date,description,image,issue_number,volume' + 
-             '&limit=12' + 
-             '&sort=cover_date:desc' + 
+             '&limit=' + settings.limit + 
+             /* TODO: pagination */
+             '&sort=store_date:desc' + 
              '&format=jsonp' + 
              '&json_callback=?', 
-        success: function(response) {
-          $('#content-loading').hide();
+        success: settings.callback /* TODO: handle errors sent back by API */
+      });
+    }, 
+    
+    /* get an issue by its ID */
+    getIssue: function(options) {
+      var settings = $.extend({
+        callback: $.noop
+      }, options || {});
+      
+      $.ajax({
+        dataType: 'jsonp', 
+        url: 'http://www.comicvine.com/api/issue/' + settings.issueId + 
+             '?api_key=e21e187f67061d346687aeb81969a3fd2d1676fa' + 
+             '&field_list=store_date,description,image,issue_number,person_credits,volume' + 
+             '&format=jsonp' + 
+             '&json_callback=?', 
+        success: settings.callback /* TODO: handle errors sent back by API */
+      });
+    }
+  };
+  
+  /* helpers */
+  myWednesdayFix.utils = {
+    loadView: function(newView) {
+      myWednesdayFix.view[newView]();
+    }, 
+    
+    showLoading: function() {
+      $('#content-loading').show();
+    }, 
+    
+    hideLoading: function() {
+      $('#content-loading').hide();
+    }, 
+    
+    setHeadline: function(headlineText) {
+      $('#content-headline').html(headlineText);
+    }, 
+    
+    resetContent: function() {
+      $('#content-wrap').html('');
+    }
+  };
+
+  /* ui methods */
+  myWednesdayFix.ui = {
+    buildIssuesList: function(options) {
+      var settings = $.extend({
+        issuesList: [], 
+        dateFilter: 'current'
+      }, options || {});
+      
+      /* TODO: pagination */
+      $.each(settings.issuesList, function() {
+        var today = new Date(), 
+        storeDateParts = this.store_date.split('-'), 
+        storeDateYear = storeDateParts[0], 
+        storeDateMonth = storeDateParts[1], 
+        storeDateDate = storeDateParts[2], 
+        storeDate = new Date(storeDateYear, 
+                             storeDateMonth - 1, 
+                             storeDateDate, 
+                             today.getHours(), 
+                             today.getMinutes(), 
+                             today.getSeconds(), 
+                             today.getMilliseconds()), 
+        daysUntilStoreDate = (storeDate - today) / 86400000, 
+        storeDateIsThisWeek = -1 < (today.getDay() + daysUntilStoreDate) && (today.getDay() + daysUntilStoreDate) < 7;
+        
+        if((settings.dateFilter === 'current' && storeDateIsThisWeek) || 
+           (settings.dateFilter === 'future' && daysUntilStoreDate > 0 && !storeDateIsThisWeek)) {
+          /* add a row if this is the first comic, or if the previous row is full */
+          if($('#content-wrap .row').length === 0 || 
+             $('#content-wrap .row:last .feature-col').length === 3) {
+            $('#content-wrap').append('<div class="row" />');
+          }
           
-          var issues = response.results;
-          $.each(issues, function() {
-            console.log(this.store_date);
-            var storeDate = this.store_date.split('-'), 
-            today = new Date(), 
-            nextPossibleWednesday = new Date();
-            nextPossibleWednesday.setDate(today.getDate() + (3 - today.getDay()));
-            
-            if(storeDate[0] == nextPossibleWednesday.getFullYear() && 
-               storeDate[1] == (nextPossibleWednesday.getMonth() + 1) && 
-               storeDate[2] == nextPossibleWednesday.getDate()) {
-              if($('#content-wrap .row').length === 0 || 
-                 $('#content-wrap .row:last .feature-col').length === 3) {
-                $('#content-wrap').append('<div class="row" />');
-              }
-              
-              $('#content-wrap .row:last').append('<div class="col-sm-4 feature-col">' + 
-                                                    '<div class="panel panel-info">' + 
-                                                      '<div class="panel-heading">' + 
-                                                        '<h3 class="panel-title">' + 
-                                                          '<a href="#">' + /* TODO: link */
-                                                            this.volume.name + ' #' + this.issue_number + 
-                                                          '</a>' + 
-                                                        '</h3>' + 
-                                                      '</div>' + 
-                                                      '<div class="panel-body">' + 
-                                                        '<div class="feature-image-wrap">' + 
-                                                          '<a href="#"><img alt="" src="' + this.image.small_url + '"></a>' + /* TODO: link */
-                                                        '</div>' + 
-                                                        this.description + /* TODO: set max length of description */
-                                                        '<p><a class="btn btn-primary" href="#" role="button">Read more &raquo;</a></p>' + /* TODO: link */
-                                                      '</div>' + 
-                                                    '</div>' + 
-                                                  '</div>');
-            }
+          var issueId = this.api_detail_url.split('/issue/')[1].split('/')[0], 
+          volumeName = this.volume.name, 
+          issueNumber = this.issue_number;
+          
+          $('#content-wrap .row:last').append('<div class="col-sm-4 feature-col">' + 
+                                                '<div class="panel panel-info">' + 
+                                                  '<div class="panel-heading">' + 
+                                                    '<h3 class="panel-title">' + 
+                                                      '<a class="view-issue" href="#issue=' + issueId + '" data-issue="' + issueId + '">' + 
+                                                        volumeName + 
+                                                        (issueNumber ? (' #' + issueNumber) : '') + 
+                                                      '</a>' + 
+                                                    '</h3>' + 
+                                                  '</div>' + 
+                                                  '<div class="panel-body">' + 
+                                                    (this.image.small_url ? 
+                                                     ('<div class="feature-image-wrap">' + 
+                                                        '<a href="#"><img alt="" src="' + this.image.small_url + '"></a>' + /* TODO: link */
+                                                      '</div>') : '') + 
+                                                    '<h3>In Stores ' + storeDateMonth + '/' + storeDateDate + '/' + storeDateYear + '</h3>' + 
+                                                    (this.description || '') + /* TODO: set max length of description */
+                                                    '<p>' + 
+                                                      '<a class="btn btn-primary view-issue" href="#issue=' + issueId + '" data-issue="' + issueId + '">' + 
+                                                        'Read more &raquo;' + 
+                                                      '</a>' + 
+                                                    '</p>' + 
+                                                  '</div>' + 
+                                                '</div>' + 
+                                              '</div>');
+        }
+      });
+    }, 
+    
+    buildIssue: function(options) {
+      var settings = $.extend({
+        issue: {}
+      }, options || {}), 
+      issue = settings.issue, 
+      volumeName = issue.volume.name, 
+      issueNumber = issue.issue_number, 
+      storeDate = issue.store_date.split('-'), 
+      storeDateYear = storeDate[0], 
+      storeDateMonth = storeDate[1], 
+      storeDateDate = storeDate[2];
+      
+      /* TODO: add back to list button */
+      
+      $('#content-wrap').append('<div class="row">' + 
+                                  '<div class="col-xs-12 feature-col">' + 
+                                    '<div class="panel panel-info">' + 
+                                      '<div class="panel-heading">' + 
+                                        '<h3 class="panel-title">' + 
+                                          volumeName + 
+                                          (issueNumber ? (' #' + issueNumber) : '') +  
+                                        '</h3>' + 
+                                      '</div>' + 
+                                      '<div class="panel-body">' + 
+                                        (issue.image.medium_url ? 
+                                         ('<div class="col-md-6 feature-image-wrap">' + 
+                                            '<a href="#"><img alt="" src="' + issue.image.medium_url + '"></a>' + /* TODO: link */
+                                          '</div>') : '') + 
+                                        '<h3>In Stores ' + storeDateMonth + '/' + storeDateDate + '/' + storeDateYear + '</h3>' + 
+                                        (issue.description || '') + 
+                                      '</div>' + 
+                                    '</div>' + 
+                                  '</div>' + 
+                                '</div>');
+      
+      $.each(issue.person_credits, function(personIndex) {
+        $('#content-wrap .panel-body').append((personIndex > 0 ? '<br>' : '') + 
+                                              '<span class="issue-credits">' + 
+                                                this.name + (this.role ? (', ' + this.role) : '') + 
+                                              '</span>');
+      });
+      
+      /* TODO: add back to list button */
+    }
+  };
+  
+  /* view handlers */
+  myWednesdayFix.view = {
+    findStore: function() {
+      myWednesdayFix.utils.setHeadline('Find a Store');
+      
+      myWednesdayFix.utils.resetContent();
+      
+      /* TODO */
+    }, 
+    
+    thisWeek: function() {
+      myWednesdayFix.utils.setHeadline('New This Week');
+      
+      myWednesdayFix.utils.resetContent();
+      
+      myWednesdayFix.utils.showLoading();
+      
+      myWednesdayFix.comicVine.getIssuesByStoreDate({
+        limit: '100', 
+        callback: function(response) {
+          myWednesdayFix.utils.hideLoading();
+          
+          myWednesdayFix.ui.buildIssuesList({
+            issuesList: response.results
           });
         }
       });
     }, 
     
-    findStore: function() {
-      $('#content-headline').html('Find a Store');
+    comingSoon: function() {
+      myWednesdayFix.utils.setHeadline('Coming Soon');
       
-      $('#content-wrap').html('');
+      myWednesdayFix.utils.resetContent();
       
-      /* TODO */
+      myWednesdayFix.utils.showLoading();
+      
+      myWednesdayFix.comicVine.getIssuesByStoreDate({
+        callback: function(response) {
+          myWednesdayFix.utils.hideLoading();
+          
+          myWednesdayFix.ui.buildIssuesList({
+            issuesList: response.results, 
+            dateFilter: 'future'
+          });
+        }
+      });
+    }, 
+    
+    viewIssue: function() {
+      myWednesdayFix.utils.setHeadline('');
+      
+      myWednesdayFix.utils.resetContent();
+      
+      myWednesdayFix.utils.showLoading();
+      
+      myWednesdayFix.comicVine.getIssue({
+        issueId: myWednesdayFix.data.currentIssue.issueId, 
+        callback: function(response) {
+          myWednesdayFix.utils.hideLoading();
+          
+          myWednesdayFix.ui.buildIssue({
+            issue: response.results
+          });
+        }
+      });
     }
   };
   
@@ -87,7 +263,16 @@
     /* TODO: prevent default */
     
     /* TODO: don't reload if view is already active, just scroll to top */
-    myWednesdayFix.view[$(this).data('view')]();
+    myWednesdayFix.utils.loadView($(this).data('view'));
+  });
+  
+  /* handle onclick event for links that load an issue */
+  $('#content-wrap').on('click', '.view-issue', function(e) {
+    /* TODO: prevent default */
+    
+    /* TODO: don't reload if view is already active, just scroll to top */
+    myWednesdayFix.data.currentIssue.issueId = $(e.target).data('issue');
+    myWednesdayFix.utils.loadView('viewIssue');
   });
   
   $(function() {
